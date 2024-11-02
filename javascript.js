9b7852d0b8ec8ab5bddf5413e0f1e8ff92faf24f
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-02 09:12:25 +0100

    assignment done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-alert('works');
+const input = document.querySelector('#item');
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+function buttonClicked() {
+  const userInput = input.value;
+  input.value = '';
+
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const button = document.createElement('button');
+
+  li.appendChild(span);
+  li.appendChild(button);
+
+  span.innerText = userInput;
+  button.innerText = 'DELETE';
+
+  ul.appendChild(li);
+
+  button.addEventListener('click', function() {
+    let li = this.parentNode;
+    li.remove();
+  });
+
+  input.focus();
+}
